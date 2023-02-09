9bc52d99d291f4b8d27478206d67344f885189f4
double7 <y775392901@icloud.com>
2023-02-09 16:02:17 +0800

    fix type hint; fix bugs

--- pydec/overrides.py
+++ pydec/overrides.py
@@ -3,16 +3,54 @@ import pydec
 import warnings
 import torch.overrides as overrides
 import functools
-from typing import Any, Dict, Union, List, Tuple, Sequence, Optional, Callable, overload
+from typing import (
+    Any,
+    Dict,
+    Union,
+    List,
+    Tuple,
+    Sequence,
+    Optional,
+    Callable,
+    TypeVar,
+    overload,
+)
+
+T = TypeVar("T")
 
 _HANDLED_BUILTIN_FUNCTIONS = {}
 _HANDLED_CUSTOMIZED_FUNCTIONS = {}
 _HANDLED_COMPOSITION_METHODS = {}
 
 
-def _register_function(torch_function: Callable, builtin: bool = False):
+_namespaces_map = {
+    "pydec._composition": torch.Tensor,
+    "pydec.variable_functions": torch,
+    "pydec.nn": torch.nn,
+    "pydec.nn.functional": torch.nn.functional,
+}
+
+"""
+Functions that are not in the list of overridable functions, but can still
+be dispatched via `__torch_function__`.
+"""
+_whitelist_torch_functions = [
+    torch.detach_,
+    torch.round_,
+    torch.abs_,
+    torch.sigmoid_,
+    torch.tanh_,
+    torch.nn.functional.relu_,
+    torch.nn.functional.leaky_relu_,
+]
+
+
+def _register_function(torch_function: T, builtin: bool = False) -> Callable[[T], T]:
     # checking
-    if torch_function not in overrides.get_testing_overrides():
+    if (
+        torch_function not in overrides.get_testing_overrides()
+        and torch_function not in _whitelist_torch_functions
+    ):
         msg = (
             f"function {torch_function} is not overridable."
             " Invoke `torch.overrides.get_overridable_functions()` to see all overridable torch functions."
@@ -26,44 +64,42 @@ def _register_function(torch_function: Callable, builtin: bool = False):
             overridable_functions = overrides.get_overridable_functions()
             if torch_function in overridable_functions[torch.Tensor]:
                 _HANDLED_COMPOSITION_METHODS[torch_function.__name__] = func
-            elif torch_function in overridable_functions[torch]:
-                setattr(pydec, torch_function.__name__, func)
-            elif torch_function in overridable_functions[torch.nn]:
-                setattr(pydec.nn, torch_function.__name__, func)
-            elif torch_function in overridable_functions[torch.nn.functional]:
-                setattr(pydec.nn.functional, torch_function.__name__, func)
-            else:
-                module_name = ".".join(
-                    ["pydec"] + overrides.resolve_name(torch_function).split(".")[1:-1]
-                )
-                warnings.warn(
-                    f"function {torch_function} will not append to pydec cause the module {module_name} doesn't exist now.",
-                    stacklevel=2,
-                )
+                if hasattr(pydec.Composition, torch_function.__name__):
+                    warnings.warn(
+                        f"pydec.Composition already has built-in method ({torch_function.__name__}), and will be overrided by {func}.",
+                        stacklevel=2,
+                    )
+            # else:
+            # pydec_modules = [pydec, pydec.nn, pydec.nn.functional]
+            # module_name = ".".join(
+            #     ["pydec"] + overrides.resolve_name(torch_function).split(".")[1:-1]
+            # )
+            # if module_name in [m.__name__ for m in pydec_modules]:
+            #     for module in pydec_modules:
+            #         if module.__name__ == module_name:
+            #             setattr(module, torch_function.__name__, func)
+            #             break
+            # else:
+            #     warnings.warn(
+            #         f"function {torch_function} will not append to pydec cause the module {module_name} doesn't exist now.",
+            #         stacklevel=2,
+            #     )
             _HANDLED_CUSTOMIZED_FUNCTIONS[torch_function] = func
         return func
 
     return _decorator
 
 
-def _register_builtin_function(torch_function: Callable):
+def _register_builtin_function(torch_function: T) -> Callable[[T], T]:
     return _register_function(torch_function, builtin=True)
 
 
-_namespaces_map = {
-    "pydec._composition": torch.Tensor,
-    "pydec.variable_functions": torch,
-    "pydec.nn": torch.nn,
-    "pydec.nn.functional": torch.nn.functional,
-}
-
-
-def register_torch_function(torch_function: Callable):
+def register_torch_function(torch_function: T) -> Callable[[T], T]:
     """Register a torch function override for Composition"""
     return _register_function(torch_function, builtin=False)
 
 
-def _auto_registration(func: Callable):
+def _auto_registration(func: T) -> T:
     assert hasattr(func, "__module__")
     assert func.__module__ in _namespaces_map
     torch_namespace = _namespaces_map[func.__module__]
@@ -78,7 +114,7 @@ def _auto_registration(func: Callable):
     return _register_function(torch_function, builtin=True)(func)
 
 
-def _hadle_self_is_tensor(func: Callable, inplace=False):
+def _hadle_self_is_tensor(func: T, inplace=False) -> T:
     # TODO: inplace warning
 
     @functools.wraps(func)
@@ -111,7 +147,7 @@ def _hadle_self_is_tensor(func: Callable, inplace=False):
     return wrapped_func
 
 
-def _hadle_self_is_tensor_inplace(func: Callable):
+def _hadle_self_is_tensor_inplace(func: T) -> T:
     def wrapped_hadle_self_is_tensor(func: Callable):
         return _hadle_self_is_tensor(func, inplace=True)
 
